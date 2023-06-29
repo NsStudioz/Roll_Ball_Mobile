f94c8682fed5c09f7b62fc57583e2aa06c9cbe9c
NsStudioz <sneve2016@gmail.com>
2023-06-29 14:24:34 +0300

    New: BlockMoveX.cs & BlockMoveY.cs to replace Block_H_Scroll.cs & Block_V_Scroll.cs

---
+++ Assets/Scripts/Blocks/New/BlockMoveX.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMoveX : MonoBehaviour
+{
+
+    [Header("Elements")]
+    // bool:
+    [SerializeField] private bool isMovingRight;
+    //
+    [SerializeField] private float moveRate;
+    [SerializeField] private float range;
+    [SerializeField] private float rangeLimit;
+
+
+    void Update()
+    {
+        SetMovementState(isMovingRight);
+        CalculateDurationOfState();
+    }
+
+    private void CalculateDurationOfState()
+    {
+        if (range >= rangeLimit)
+        {
+            isMovingRight = !isMovingRight;
+            range = 0;
+        }
+    }
+
+    private void SetMovementState(bool state)
+    {
+        if (state)
+            Move(moveRate, Time.deltaTime);
+        else
+            Move(-moveRate, Time.deltaTime);
+    }
+
+    private void Move(float moveRate, float deltaTime)
+    {
+        float xDirection = moveRate * deltaTime;
+        transform.Translate(new Vector2(xDirection, 0f));
+        range += Mathf.Abs(xDirection);
+    }
+}
