b6d5bceb38ebf9851d59a0f8c82f2348428c7842
NsStudioz <sneve2016@gmail.com>
2023-06-29 10:46:38 +0300

    Refactor: New Blackhole trap movement scripts.

---
+++ Assets/Scripts/Traps/New/Trap_BlackHoleY.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_BlackHoleY : MonoBehaviour
+{
+
+    [Header("Elements")]
+    // bool:
+    [SerializeField] private bool isMovingUp;
+    //
+    [SerializeField] private float moveRate;
+    [SerializeField] private float range;
+    [SerializeField] private float rangeLimit;
+
+
+    void Update()
+    {
+        SetMovementState(isMovingUp);
+        CalculateDurationOfState();
+    }
+
+    private void CalculateDurationOfState()
+    {
+        if (range >= rangeLimit)
+        {
+            isMovingUp = !isMovingUp;
+            range = 0;
+        }
+    }
+
+    private void SetMovementState(bool state)
+    {
+        if (state)
+            Move(moveRate, Time.deltaTime);
+        else
+            Move(-moveRate, Time.deltaTime);
+    }
+
+    private void Move(float moveRate, float deltaTime)
+    {
+        float yDirection = moveRate * deltaTime;
+        transform.Translate(new Vector2(0f, yDirection));
+        range += Mathf.Abs(yDirection);
+    }
+}
