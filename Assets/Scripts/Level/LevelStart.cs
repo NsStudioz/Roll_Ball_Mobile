d349965c45edd80cf26095fa4e5cb50825925206
NsStudioz <sneve2016@gmail.com>
2023-06-30 11:43:12 +0300

    Refactor: StartingPoint.cs => LevelStart.cs

--- Assets/Scripts/Level/LevelStart.cs
+++ Assets/Scripts/Level/LevelStart.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartingPoint : MonoBehaviour
+public class LevelStart : MonoBehaviour
 {
 
-    TimerScript timerScript;
+/*    TimerScript timerScript;
 
     void Start()
     {
         GameObject timesUp = GameObject.Find("Gamesession");
         timerScript = timesUp.GetComponent<TimerScript>();
-    }
+    }*/
 
     private void OnTriggerExit2D(Collider2D player)
     {
-        timerScript.startTimer = true;
+        //timerScript.startTimer = true;
+        PlayerEvents.OnLevelStarted?.Invoke();
     }
 
 }
