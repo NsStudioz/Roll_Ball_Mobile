0f69e034a98cf1d632a712b010440b62f1cf68d2
NsStudioz <sneve2016@gmail.com>
2023-06-30 11:45:29 +0300

    Update: PlayerEvents.cs

--- Assets/Scripts/Player/PlayerEvents.cs
+++ Assets/Scripts/Player/PlayerEvents.cs
@@ -6,23 +6,37 @@ using UnityEngine;
 public class PlayerEvents
 {
 
-    public delegate void PlayerJump();
-    public static PlayerJump OnPlayerJump;
-
-    public delegate void PlayerDead();
-    public static PlayerDead OnPlayerDead;
+    public delegate void LevelStarted();
+    public static LevelStarted OnLevelStarted;
 
     public delegate void LevelCompleted();
     public static LevelCompleted OnLevelCompleted;
 
-    public delegate void TimeOut();
-    public static TimeOut OnTimeOut;
+    public delegate void LevelReset();
+    public static LevelReset OnLevelReset;
 
-/*    public delegate void PlayerDead();
-    public static PlayerDead OnPlayerDead;
+    public delegate void OutOfTime();
+    public static OutOfTime OnOutOfTime;
 
     public delegate void PlayerDead();
-    public static PlayerDead OnPlayerDead;*/
+    public static PlayerDead OnPlayerDead;
+    //
+    public delegate void KeyPickup();
+    public static KeyPickup OnKeyPickup;
+
+    public delegate void KeyUsed();
+    public static KeyUsed OnKeyUsed;
+
+    public delegate void PlayerJump();
+    public static PlayerJump OnPlayerJump;
+
+    public delegate void JumpPickup();
+    public static JumpPickup OnJumpPickup;
+
+    public delegate void TimePickup(float time);
+    public static TimePickup OnTimePickup;
+
+
 
 
 }
