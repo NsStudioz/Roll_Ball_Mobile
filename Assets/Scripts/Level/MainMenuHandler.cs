54758c46501c2fecbdbfda1ad478dfdc12df5070
NsStudioz <sneve2016@gmail.com>
2023-07-03 01:18:15 +0300

    Update: MainMenuHandler.cs = less visible now.

--- Assets/Scripts/Level/MainMenuHandler.cs
+++ Assets/Scripts/Level/MainMenuHandler.cs
@@ -1,65 +1,67 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuHandler : MonoBehaviour
+namespace MainMenu
 {
-
-    [Header("Menu Scene Indexers")]
-    [SerializeField] private int MAIN_MENU_INDEX = 1;
-    [SerializeField] private int LEVEL_SELECT_INDEX = 2;
-    [SerializeField] private int OPTIONS_INDEX = 53;
-    [SerializeField] private int CREDITS_INDEX = 54;
-    [SerializeField] private int TUTORIAL_INDEX = 55;
-
-    public void StartGame()
+    public class MainMenuHandler : MonoBehaviour
     {
-        LoadScene(LEVEL_SELECT_INDEX);
-        PlayAudioClipOnButtonClick();
+
+        [Header("Menu Scene Indexers")]
+        [SerializeField] private int MAIN_MENU_INDEX = 1;
+        [SerializeField] private int LEVEL_SELECT_INDEX = 2;
+        [SerializeField] private int OPTIONS_INDEX = 53;
+        [SerializeField] private int CREDITS_INDEX = 54;
+        [SerializeField] private int TUTORIAL_INDEX = 55;
+
+        public void StartGame()
+        {
+            LoadScene(LEVEL_SELECT_INDEX);
+            PlayAudioClipOnButtonClick();
+        }
+
+        public void ReturnToMainMenu()
+        {
+            LoadScene(MAIN_MENU_INDEX);
+            PlayAudioClipOnBackButtonClick();
+        }
+
+        public void OptionsMenu()
+        {
+            LoadScene(OPTIONS_INDEX);
+            PlayAudioClipOnButtonClick();
+        }
+
+        public void CreditsMenu()
+        {
+            LoadScene(CREDITS_INDEX);
+            PlayAudioClipOnButtonClick();
+        }
+
+        public void TutorialScene()
+        {
+            LoadScene(TUTORIAL_INDEX);
+            PlayAudioClipOnButtonClick();
+        }
+
+        private void LoadScene(int SceneIndex)
+        {
+            SceneManager.LoadScene(SceneIndex);
+        }
+
+        private void PlayAudioClipOnButtonClick()
+        {
+            AudioManager.Instance.Play("ButtonClick");
+        }
+
+        private void PlayAudioClipOnBackButtonClick()
+        {
+            AudioManager.Instance.Play("BackButtonClick");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
-
-    public void ReturnToMainMenu()
-    {
-        LoadScene(MAIN_MENU_INDEX);
-        PlayAudioClipOnBackButtonClick();
-    }
-
-    public void OptionsMenu()
-    {
-        LoadScene(OPTIONS_INDEX);
-        PlayAudioClipOnButtonClick();
-    }
-
-    public void CreditsMenu()
-    {
-        LoadScene(CREDITS_INDEX);
-        PlayAudioClipOnButtonClick();
-
-    }
-
-    public void TutorialScene()
-    {
-        LoadScene(TUTORIAL_INDEX);
-        PlayAudioClipOnButtonClick();
-    }
-
-    private void LoadScene(int SceneIndex)
-    {
-        SceneManager.LoadScene(SceneIndex);
-    }
-
-    private void PlayAudioClipOnButtonClick()
-    {
-        AudioManager.Instance.Play("ButtonClick");
-    }
-
-    private void PlayAudioClipOnBackButtonClick()
-    {
-        AudioManager.Instance.Play("BackButtonClick");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
 }
+
