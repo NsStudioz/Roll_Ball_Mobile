d2c6d01ff52366210db76416377d40d24a929cc2
NsStudioz <sneve2016@gmail.com>
2023-07-02 12:59:28 +0300

    Update: LevelsHandler.cs = new fade to level mechanics.

--- Assets/Scripts/Level/New/LevelsHandler.cs
+++ Assets/Scripts/Level/New/LevelsHandler.cs
@@ -1,21 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelsHandler : MonoBehaviour
 {
 
+    public static LevelsHandler Instance;
+
     [SerializeField] private Animator animator;
+    private int levelIndex;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string fadeInAnim;
+    [SerializeField] private string fadeOutAnim;
+
+    private void Awake()
     {
-        
+        InitializeSingleton();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeSingleton()
     {
-        
+        if (Instance == null) { Instance = this; } // singleton pattern
+        else
+        {
+            Destroy(gameObject);
+            return; // so that no more code is called before we destroy this gameObject.
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
+
+    public void FadeToLevel(int currentLevelIndex)
+    {
+        levelIndex = currentLevelIndex;
+
+        animator.Play(fadeOutAnim);
+        AudioManager.Instance.Play("LevelChoosed");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelIndex);
+
+        // Music Manager swap tracks from main theme to level theme:
+    }
+
+
+
 }
+
+//animator.SetTrigger("FadeOut");
+//FindObjectOfType<AudioManager>().Play("LevelChoosed");
