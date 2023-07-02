1bbaa545abb8f418361cf5a9863926314f9ce7ba
NsStudioz <sneve2016@gmail.com>
2023-07-02 15:22:29 +0300

    Update: LevelLoader_Splash = Condensed code from 2 different scripts to one class in one script for this mechanic.

--- Assets/Scripts/Level/LevelLoader_Splash.cs
+++ Assets/Scripts/Level/LevelLoader_Splash.cs
@@ -1,25 +1,44 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader_Splash : MonoBehaviour
+
+namespace InitSplash
 {
-    public Animator animator; 
-    [SerializeField] int levelToLoad;
-
-    public void FadeToNextLevel()
+    public class LevelLoader_Splash : MonoBehaviour
     {
-        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+        public Animator animator;
+        [SerializeField] int levelToLoad;
 
-    private void FadeToLevel(int currentLevelIndex)
-    {
-        levelToLoad = currentLevelIndex; 
-        animator.SetTrigger("FadeOut");
-    }
+        [SerializeField] float timeElapesd = 0f;
+        [SerializeField] float delayTime = 5f;
 
-    // Public since this function depends on the animator:
-    public void OnFadeComplete()
-    {
-        SceneManager.LoadScene(levelToLoad);
+        void Update()
+        {
+            if (timeElapesd < delayTime)
+                timeElapesd += Time.deltaTime;
+            else
+                FadeToMainMenuScene();
+        }
+
+        private void FadeToMainMenuScene()
+        {
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private void FadeToLevel(int currentLevelIndex)
+        {
+            levelToLoad = currentLevelIndex;
+            animator.SetTrigger("FadeOut");
+        }
+
+        // Public since this function depends on the animator:
+        public void OnFadeComplete()
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
     }
 }
+
+
+
+//if (SceneManager.GetActiveScene().buildIndex < 1) { }
