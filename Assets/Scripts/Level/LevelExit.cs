c4c2eec1e787ecfab25038c9e85bb88bea7d22f6
NsStudioz <sneve2016@gmail.com>
2023-07-03 01:40:12 +0300

    Refactor (WIP): LevelExit.cs = Player Collision scene transition trigger code implemented.

--- Assets/Scripts/Level/LevelExit.cs
+++ Assets/Scripts/Level/LevelExit.cs
@@ -7,19 +7,31 @@ using UnityEngine.SceneManagement;
 public class LevelExit : MonoBehaviour
 {
 
+    private string player = "Player";
+    private int nextSceneLoad;
+
     MusicManager musicManager;
 
     private int[] musicTriggerValues = { 12, 17, 22, 27, 31, 34, 39, 42, 49, 51 };
 
     void Start()
     {
+        nextSceneLoad = GameSession.Instance.CurrentSceneIndex + 1;
+        //
         GameObject forMusicManager = GameObject.Find("MusicManager");
         musicManager = forMusicManager.GetComponent<MusicManager>();
     }
 
-    private void OnTriggerEnter2D(Collider2D player)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        GameEvents.OnLevelCompleted?.Invoke();
+        GameEvents.OnTriggerStopTimer?.Invoke();
+        AudioManager.Instance.Play("LevelCompleted");
+
+        if (other.gameObject.name == player)
+        {
+            CheckLevelProgress();
+        }
+        //GameEvents.OnLevelCompleted?.Invoke();
 
         if (musicTriggerValues.Contains(GameSession.Instance.CurrentSceneIndex))
         {
@@ -31,6 +43,24 @@ public class LevelExit : MonoBehaviour
             musicManager.SwapTracks("Theme_50", "Theme_Main_Menu");
         }
     }
+
+    private void CheckLevelProgress()
+    {
+        if (GameSession.Instance.CurrentSceneIndex == 52)
+            GameEvents.OnReturnToMainMenu?.Invoke();
+        else
+        {
+            GameEvents.OnLevelCompleted?.Invoke();   
+            SaveLevelProgress();
+        }
+    }
+
+    private void SaveLevelProgress()
+    {
+        // Setting Int for Index
+        if (nextSceneLoad > PlayerPrefs.GetInt("levelAt")) { PlayerPrefs.SetInt("levelAt", nextSceneLoad); }
+    }
+
 }
 
 
