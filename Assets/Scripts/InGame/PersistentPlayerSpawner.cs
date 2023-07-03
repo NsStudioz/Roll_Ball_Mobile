0e40f6731f9b3019e6662b4433ccb541f499d02d
NsStudioz <sneve2016@gmail.com>
2023-07-04 00:42:08 +0300

    New: PersistentPlayerSpawner.cs for Persistent player mechanics.

---
+++ Assets/Scripts/InGame/PersistentPlayerSpawner.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentPlayerSpawner : MonoBehaviour
+{
+
+    public static PersistentPlayerSpawner Instance;
+
+    [SerializeField] GameObject persistentPlayerPrefab = null;
+    [SerializeField] GameObject persistentInstance = null;
+
+    private static bool hasSpawned = false;
+
+    private void Awake()
+    {
+        InitializeSingleton();
+
+        if (hasSpawned) return;
+
+        SpawnPersistentPlayerObject();
+
+        hasSpawned = true;
+    }
+    private void InitializeSingleton()
+    {
+        if (Instance == null) { Instance = this; } // singleton pattern
+        else
+        {
+            Destroy(gameObject);
+            return; // so that no more code is called before we destroy this gameObject.
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+    private void OnSceneLoaded(Scene sceneIndex, LoadSceneMode mode)
+    {
+        SetPlayerObjectVisibility();
+    }
+
+    private void SpawnPersistentPlayerObject()
+    {
+        if (persistentPlayerPrefab != null)
+        {
+            GameObject persistentObject = Instantiate(persistentPlayerPrefab);
+            persistentInstance = persistentObject;
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+
+    private void SetPlayerObjectVisibility()
+    {
+        if (persistentPlayerPrefab == null)
+            return;
+
+        if (GameSession.Instance.CurrentSceneIndex > 2 && GameSession.Instance.CurrentSceneIndex < 53)
+            persistentInstance.SetActive(true);
+        else
+            persistentInstance.SetActive(false);
+
+    }
+
+}
