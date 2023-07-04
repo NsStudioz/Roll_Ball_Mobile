552906593251588f3780c9862dd6c4faa1de2a4f
NsStudioz <sneve2016@gmail.com>
2023-07-04 11:29:34 +0300

    New: FollowPlayer.cs = cinemachine vcam now follows player.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab = null;
+    private string PLAYER_TAG = "Player";
+
+    private void Start()
+    {
+        FollowTargetPlayer();
+    }
+
+    private void FollowTargetPlayer()
+    {
+        playerPrefab = GameObject.FindGameObjectWithTag(PLAYER_TAG);
+
+        var vCamera = GetComponent<CinemachineVirtualCamera>();
+
+        vCamera.Follow = playerPrefab.transform;
+    }
+
+}
