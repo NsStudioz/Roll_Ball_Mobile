e7bd3525b40eb17cf9de31632d19ef7886d77140
NsStudioz <sneve2016@gmail.com>
2023-07-06 15:23:03 +0300

    Fix: PlatformAttach.cs = Player become destroyable on block attached. Fixed.

--- Assets/Scripts/Blocks/PlatformAttach.cs
+++ Assets/Scripts/Blocks/PlatformAttach.cs
@@ -6,22 +6,40 @@ public class PlatformAttach : MonoBehaviour
 {
 
     [SerializeField] private GameObject playerPrefab;
+    private string player = "Player";
+    private bool isOutOfTime = false;
+
+    private void OnEnable()
+    {
+        GameEvents.OnOutOfTime += TriggerOutOfTime;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnOutOfTime -= TriggerOutOfTime;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject == playerPrefab) // if this other collision is the Player
+        if (other.CompareTag(player) && !isOutOfTime) // if this other collision is the Player
         {
-            playerPrefab.transform.parent = transform; // Attach Player position to this platform.
+            other.gameObject.transform.parent = transform; // Attach Player position to this platform.
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.gameObject == playerPrefab) // if this other collision is the Player
-        {
-            playerPrefab.transform.parent = null; // Detach Player position from this platform.
-        }
+        if(other.CompareTag(player)) // if this other collision is the Player
+            other.gameObject.transform.parent = null; // Detach Player position from this platform.
+
+        else if (isOutOfTime)
+            other.gameObject.transform.parent = null;
+
+        DontDestroyOnLoad(other.gameObject);
     }
 
-
+    private void TriggerOutOfTime()
+    {
+        isOutOfTime = true;
+    }
 }
--- Assets/Scripts/UsefullCode.cs
+++ Assets/Scripts/UsefullCode.cs
@@ -136,4 +136,22 @@ public class UsefullCode
 
     #endregion
 
+
+    /*    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == playerPrefab) // if this other collision is the Player
+        {
+            playerPrefab.transform.parent = transform; // Attach Player position to this platform.
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == playerPrefab) // if this other collision is the Player
+        {
+            playerPrefab.transform.parent = null; // Detach Player position from this platform.
+        }
+    }*/
+
+
 }
