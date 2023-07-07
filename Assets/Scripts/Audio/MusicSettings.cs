44f7bb3c3a008fb78e8ec8139841f14a0af4bc73
NsStudioz <sneve2016@gmail.com>
2023-07-07 16:04:28 +0300

    Update: MusicSettings.cs = Removed unused code.

--- Assets/Scripts/Audio/MusicSettings.cs
+++ Assets/Scripts/Audio/MusicSettings.cs
@@ -7,18 +7,11 @@ using UnityEngine.UI;
 
 public class MusicSettings : MonoBehaviour
 {
-    MusicManager musicManager;
-
-    [SerializeField] GameObject musicButtonOn;
-    [SerializeField] GameObject musicButtonOff;
-
-    [SerializeField] bool m_Muted = false;
-    //
 
     [SerializeField] private Button musicMuteBtn;
     [SerializeField] private Sprite musicMutedImg;
     [SerializeField] private Sprite musicUnmutedImg;
-
+    private bool m_Muted = false;
 
     private void OnEnable()
     {
@@ -67,55 +60,14 @@ public class MusicSettings : MonoBehaviour
         OptionsEvents.OnMusicMute?.Invoke();
     }
 
-    private void Awake()
-    {
-        GameObject forMusicManager = GameObject.Find("MusicManager");
-        musicManager = forMusicManager.GetComponent<MusicManager>();
-    }
-
     void Start()
     {
         if (!PlayerPrefs.HasKey("m_Muted"))
-        {
             PlayerPrefs.SetInt("m_Muted", 0);
-        }
         else
-        {
             LoadMusicSettings();
-        }
 
-        UpdateButtonIcon();
-    }
-
-    public void MusicOn()
-    {
-        m_Muted = false;
-        FindObjectOfType<AudioManager>().Play("AudioButtonClick");
-        UpdateButtonIcon();
-        SaveMusicSettings();
-        musicManager.SetMusicSettings();
-    }
-
-    public void MusicOff()
-    {
-        m_Muted = true;
-        UpdateButtonIcon();
-        SaveMusicSettings();
-        musicManager.SetMusicSettings();
-    }
-
-    private void UpdateButtonIcon()
-    {
-        if (m_Muted == false)
-        {
-            musicButtonOn.SetActive(true);
-            musicButtonOff.SetActive(false);
-        }
-        else
-        {
-            musicButtonOn.SetActive(false);
-            musicButtonOff.SetActive(true);
-        }
+        UpdateButtonImage();
     }
 
     private void LoadMusicSettings()
@@ -127,5 +79,4 @@ public class MusicSettings : MonoBehaviour
     {
         PlayerPrefs.SetInt("m_Muted", m_Muted ? 1 : 0);
     }
-
 }
