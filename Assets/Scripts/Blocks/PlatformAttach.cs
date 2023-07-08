ffa15d9f180fbef3df41ef082adf7753939754e0
NsStudioz <sneve2016@gmail.com>
2023-07-08 22:41:58 +0300

    Fix: PlatformAttach.cs = Player bugs out of hierarchy when attached to a platform and restart level. Fixed

--- Assets/Scripts/Blocks/PlatformAttach.cs
+++ Assets/Scripts/Blocks/PlatformAttach.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +7,68 @@ public class PlatformAttach : MonoBehaviour
 {
 
     private readonly string player = "Player";
-    private bool isOutOfTime = false;
+    private bool detachFromPlatform = false;
 
     private void OnEnable()
     {
-        GameEvents.OnOutOfTime += OnOutOfTimeInvoked_TriggerOutOfTime;
+        GameEvents.OnOutOfTime += OnInvoked_DetachPlayerFromPlatform;
+        GameEvents.OnPlayerDead += OnInvoked_DetachPlayerFromPlatform;
+        UIEvents.OnPause += OnPause_PlayerPlatformDetach;
+        UIEvents.OnResume += OnResume_PlayerPlatformAttach;
     }
+
+
 
     private void OnDisable()
     {
-        GameEvents.OnOutOfTime -= OnOutOfTimeInvoked_TriggerOutOfTime;
+        GameEvents.OnOutOfTime -= OnInvoked_DetachPlayerFromPlatform;
+        GameEvents.OnPlayerDead -= OnInvoked_DetachPlayerFromPlatform;
+        UIEvents.OnPause -= OnPause_PlayerPlatformDetach;
+        UIEvents.OnResume -= OnResume_PlayerPlatformAttach;
+    }
+    private void OnResume_PlayerPlatformAttach() => SetDetachFromPlatform(false);
+
+    private void OnPause_PlayerPlatformDetach()
+    {
+        SetDetachFromPlatform(true);
+        DetachPlayer();
+    }
+
+    private void DetachPlayer()
+    {
+        GameObject playerSingleton = GameObject.FindGameObjectWithTag(player);
+
+        if (playerSingleton != null && detachFromPlatform)
+        {
+            playerSingleton.gameObject.transform.parent = null;
+            DontDestroyOnLoad(playerSingleton);
+        }
+    }
+
+    private void OnInvoked_DetachPlayerFromPlatform() => SetDetachFromPlatform(true);
+
+    private void SetDetachFromPlatform(bool state)
+    {
+        detachFromPlatform = state;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag(player) && !isOutOfTime) // if this other collision is the Player
+        if (other.CompareTag(player) && !detachFromPlatform) // if this other collision is the Player
             other.gameObject.transform.parent = transform; // Attach Player position to this platform.
+        else if(other.CompareTag(player) && detachFromPlatform)
+        {
+            other.gameObject.transform.parent = null;
+            DontDestroyOnLoad(other.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag(player) && !detachFromPlatform)
+        {
+            other.gameObject.transform.parent = transform;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -29,14 +76,22 @@ public class PlatformAttach : MonoBehaviour
         if(other.CompareTag(player)) // if this other collision is the Player
             other.gameObject.transform.parent = null; // Detach Player position from this platform.
 
-        else if (other.CompareTag(player) && isOutOfTime)
+        else if (other.CompareTag(player) && detachFromPlatform)
             other.gameObject.transform.parent = null;
 
         DontDestroyOnLoad(other.gameObject);
     }
 
-    private void OnOutOfTimeInvoked_TriggerOutOfTime()
+    private void SetPlayerObjectParentToRoot(Collider2D player)
     {
-        isOutOfTime = true;
+        player.gameObject.transform.parent = null;
     }
+
+    private void SetPlayerObjectParentToPlatformObject(Collider2D player)
+    {
+        player.gameObject.transform.parent = transform;
+    }
+
+
+
 }
